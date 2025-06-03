a851e8014fde596f6663a47cab19c3b2dcbdbe0e
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-06-03 15:41:13 +0200

    WIP

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -7,13 +7,41 @@ import { Component, TrackByFunction } from '@angular/core';
   styleUrl: './book-list.component.css',
 })
 export class BookListComponent {
+  imageWidth = 50;
+  coverIsVisible = true;
+
   books = [
-    { isbn: '123456789', title: 'Angular 19', price: 29.99, rating: 4 },
-    { isbn: '123456790', title: 'Angular 20', price: 39.99, rating: 4.5 },
-    { isbn: '123456791', title: 'PHP', price: 19.99, rating: 3.5 },
+    {
+      isbn: '123456789',
+      title: 'Angular 19',
+      price: 29.99,
+      rating: 4,
+      coverUrl:
+        'https://m.media-amazon.com/images/W/MEDIAX_1215821-T2/images/I/71rUdxFkaCL._AC_UY436_QL65_.jpg',
+    },
+    {
+      isbn: '123456790',
+      title: 'Angular 20',
+      price: 39.99,
+      rating: 4.5,
+      coverUrl:
+        'https://m.media-amazon.com/images/W/MEDIAX_1215821-T2/images/I/61YTE17hNYL._AC_UY436_QL65_.jpg',
+    },
+    {
+      isbn: '123456791',
+      title: 'PHP',
+      price: 9.99,
+      rating: 3.5,
+      coverUrl:
+        'https://m.media-amazon.com/images/W/MEDIAX_1215821-T2/images/I/71S1L2AntIL._AC_UY436_QL65_.jpg',
+    },
   ];
 
   trackByIsbn(book: any): string {
     return book.isbn;
+  }
+
+  toggleCover() {
+    this.coverIsVisible = !this.coverIsVisible;
   }
 }
