fb646667c5b15ec025c4a0ab01a80936f6d21536
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-06-04 15:36:41 +0200

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { WelcomeComponent } from './books/welcome/welcome.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'books/:isbn', component: BookDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -3,7 +3,7 @@ import { Book } from '../book';
 import { BookDataService } from '../book-data.service';
 
 @Component({
-  selector: 'book-list',
+  //  selector: 'book-list',
   standalone: false,
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css',
