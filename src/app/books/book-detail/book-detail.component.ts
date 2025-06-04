0138e6bcf6c06bde99fa69bad3090374a65467dd
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-06-04 16:12:14 +0200

    WIP

--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookDataService } from '../book-data.service';
+import { Book } from '../book';
 
 @Component({
   selector: 'book-book-detail',
   standalone: false,
   templateUrl: './book-detail.component.html',
-  styleUrl: './book-detail.component.css'
+  styleUrl: './book-detail.component.css',
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit {
+  public isbn = '';
+  public book: Book | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private bookDataService: BookDataService
+  ) {}
+
+  public async ngOnInit(): Promise<void> {
+    this.isbn = this.route.snapshot.params['isbn'];
+    this.book = await this.bookDataService.getBook(this.isbn);
+  }
 }
