f5c960c6e73276b10c1d77ae2c9106c4766f3cff
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-06-05 12:24:02 +0200

    WIP

--- src/app/books/welcome/welcome.component.ts
+++ src/app/books/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of, timer } from 'rxjs';
+import { filter, map, of, Subject, timer } from 'rxjs';
 
 @Component({
   standalone: false,
@@ -10,10 +10,29 @@ export class WelcomeComponent {
   constructor() {
     //
     const obs1$ = timer(0, 500);
-    obs1$.subscribe({
-      next: (value) => console.log('Next:', value),
-      error: (error) => console.error('Error:', error),
-      complete: () => console.log('Completed'),
-    });
+    const obs2$ = obs1$.pipe(filter((value) => value > 10));
+    const obs3$ = obs2$.pipe(map((value) => value * 2));
+
+    // obs3$.subscribe({
+    //   next: (value) => console.log('Next:', value),
+    //   error: (error) => console.error('Error:', error),
+    //   complete: () => console.log('Completed'),
+    // });
+
+    const mySubject$ = new Subject<string>();
+    mySubject$.next('Hallo');
+
+    mySubject$.subscribe((value) =>
+      console.log('Nachricht aus dem Subject 1:', value)
+    );
+
+    mySubject$.next('Welt');
+
+    mySubject$.subscribe((value) =>
+      console.log('Nachricht aus dem Subject 2:', value)
+    );
+
+    mySubject$.next('Ende der Welt');
+    mySubject$.complete();
   }
 }
