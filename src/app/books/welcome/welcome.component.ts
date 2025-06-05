918adfd0f3bfb1a6005e1795292d2107c5feec90
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-06-05 13:55:20 +0200

    WIP

--- src/app/books/welcome/welcome.component.ts
+++ src/app/books/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { filter, map, of, Subject, timer } from 'rxjs';
+import { interval, Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   standalone: false,
@@ -7,32 +7,50 @@ import { filter, map, of, Subject, timer } from 'rxjs';
   styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
+  //  public mySubscriptions$: Subscription[] = [];
+  public destroy$ = new Subject();
+
   constructor() {
     //
-    const obs1$ = timer(0, 500);
-    const obs2$ = obs1$.pipe(filter((value) => value > 10));
-    const obs3$ = obs2$.pipe(map((value) => value * 2));
-
+    // const obs1$ = timer(0, 500);
+    // const obs2$ = obs1$.pipe(filter((value) => value > 10));
+    // const obs3$ = obs2$.pipe(map((value) => value * 2));
     // obs3$.subscribe({
     //   next: (value) => console.log('Next:', value),
     //   error: (error) => console.error('Error:', error),
     //   complete: () => console.log('Completed'),
     // });
+    // const mySubject$ = new Subject<string>();
+    // mySubject$.next('Hallo');
+    // mySubject$.subscribe((value) =>
+    //   console.log('Nachricht aus dem Subject 1:', value)
+    // );
+    // mySubject$.next('Welt');
+    // mySubject$.subscribe((value) =>
+    //   console.log('Nachricht aus dem Subject 2:', value)
+    // );
+    // mySubject$.next('Ende der Welt');
+    // mySubject$.complete();
+  }
 
-    const mySubject$ = new Subject<string>();
-    mySubject$.next('Hallo');
+  ngOnInit() {
+    // this.mySubscriptions$.push(
+    //   interval(1000).subscribe((daten) => {
+    //     console.log('Daten:', daten);
+    //   })
+    // );
 
-    mySubject$.subscribe((value) =>
-      console.log('Nachricht aus dem Subject 1:', value)
-    );
+    interval(1000)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((x) => console.log(x));
+  }
 
-    mySubject$.next('Welt');
-
-    mySubject$.subscribe((value) =>
-      console.log('Nachricht aus dem Subject 2:', value)
-    );
-
-    mySubject$.next('Ende der Welt');
-    mySubject$.complete();
+  ngOnDestroy() {
+    // this.mySubscriptions$.forEach((subscription) => {
+    //   if (subscription) {
+    //     subscription.unsubscribe();
+    //   }
+    // });
+    this.destroy$.next('bumm.');
   }
 }
