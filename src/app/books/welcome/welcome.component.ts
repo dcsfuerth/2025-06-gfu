cc69abc05e2457caa77f5e0a966595dada20591e
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-06-05 11:58:39 +0200

    WIP

--- src/app/books/welcome/welcome.component.ts
+++ src/app/books/welcome/welcome.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { of, timer } from 'rxjs';
 
 @Component({
   standalone: false,
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  constructor() {
+    //
+    const obs1$ = timer(0, 500);
+    obs1$.subscribe({
+      next: (value) => console.log('Next:', value),
+      error: (error) => console.error('Error:', error),
+      complete: () => console.log('Completed'),
+    });
+  }
+}
